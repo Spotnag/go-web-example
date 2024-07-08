c7fd88526a488d4651c1263a554579fd536e213c
Spotnag <spotnag@hotmail.com>
2024-07-08 09:08:46 +0100

    added make file

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-run:
-	@go run ./main.go
+run: build
+	@./bin/app
+
+build:
+	@go build -o bin/app cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -24,15 +24,35 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 
 func main() {
 	e := echo.New()
-
 	e.Renderer = newTemplate()
 	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CSRF())
+	e.Debug = true // TODO REMOVE IN PRODUCTION
+
 	e.Static("/images", "images")
 	e.Static("/css", "css")
 
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "admin" && password == "admin" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	g.GET("/dashboard", func(c echo.Context) error {
+		return c.String(200, "Admin Dashboard")
+	})
 	e.GET("/", func(c echo.Context) error {
+
 		return c.Render(200, "base.html", nil)
 	})
+	e.GET("/hello", handler)
 
-	e.Logger.Fatal(e.Start("localhost:3000"))
+	e.Logger.Fatal(e.Start("localhost:3000")) // TODO REMOVE IN PRODUCTION
+}
+
+func handler(c echo.Context) error {
+	return c.Render(200, "base.html", nil)
 }
